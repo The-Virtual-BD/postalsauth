28dbbb6146189b3a38b664e61111305e7100f35d
Hussains3 <shussain8978@gmail.com>
2023-02-15 16:25:45 +0600

    Job responsive

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -3,7 +3,7 @@
         <h1 class="font-poppins font-semibold text-4xl text-white">Jobs</h1>
     </div>
     <div class="py-6 ">
-        <div class="grid sm:grid grid-cols-3 container mx-auto py-4 gap-6">
+        <div class="grid grid-cols-1 sm:grid-cols-3 container mx-auto py-4 gap-6">
                 @foreach ($jobs as $job)
                     <div class="bg-white rounded-md drop-shadow p-4">
                         <p class="text-black font-poppins font-semibold text-lg mb-4">{{ $job->jobTitle }}</p>
